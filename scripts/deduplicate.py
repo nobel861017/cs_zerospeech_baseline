7bc6380245633f1cc779ad4227f41e789a6368bc
cky1124 <kevinyang1124@gmail.com>
2023-08-16 16:15:21 +0800

    modify for conciseness

--- scripts/deduplicate.py
+++ scripts/deduplicate.py
@@ -1,10 +1,9 @@
 import argparse
-import random
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_name')
-parser.add_argument('--output')
-parser.add_argument('--deleted_path')
+parser.add_argument('--output_dir')
 parser.add_argument('--convert', action='store_true')
 parser.add_argument('--max_units', type=int, default=512)
 
@@ -12,90 +11,51 @@ args = parser.parse_args()
 
 def preprocess(file_name, convert):
     f = open(file_name, 'r')
-    if not convert:
-        names = []
-        lines = []
-        deleted = []
-        deleted_names = []
-        for line in f.readlines():
-            line = line.split('\t')
-            wav_name, line = line[0], line[1]
-            line = line.split(',')
-            if '\n' in line[-1]:
-                line[-1] = line[-1][:-1]
-            new_line = []
-            for id in line:
-                if len(new_line)==0:
-                    new_line.append(id)
-                else:
-                    if new_line[-1]==id:
-                        continue
-                    new_line.append(id)
-            if len(new_line)<=args.max_units:
+    names = []
+    lines = []
+    deleted = []
+    deleted_names = []
+    for line in f.readlines():
+        wav_name, line = line.split('\t')
+        line = line.strip('\n').split(',')
+        new_line = []
+        for id in line:
+            if len(new_line) == 0:
+                new_line.append(id)
+            elif new_line[-1] != id:
+                new_line.append(id)
+        if len(new_line) <= args.max_units:
+            if not convert:
                 names.append(wav_name)
-                lines.append(new_line)
-            else:
-                deleted_names.append(wav_name)
-                deleted.append(new_line)
-            
-            
+            lines.append(new_line)
+        else:
+            deleted_names.append(wav_name)
+            deleted.append(new_line)
+    
+    if not convert:
         return names, lines, deleted_names, deleted
+    
+    return None, lines, deleted_names, deleted
 
-    else:
-        deleted_names = []
-        lines = []
-        deleted = []
 
-        for line in f.readlines():
-            line = line.split('\t')
-            wav_name, line = line[0], line[1]
-            line = line.split(',')
-            
-            if '\n' in line[-1]:
-                line[-1] = line[-1][:-1]
-            new_line = []
-            for id in line:
-                if len(new_line)==0:
-                    new_line.append(id)
-                else:
-                    if new_line[-1]==id:
-                        continue
-                    new_line.append(id)
-            
-            if len(new_line)<=args.max_units:
-                lines.append(new_line)
-            else:
-                deleted_names.append(wav_name)
-                deleted.append(new_line)
-            
-        return None, lines, deleted_names, deleted
 
 def write_output(names, lines, output, convert):
     if not convert:
         with open(output, 'w') as f:
             assert len(names)==len(lines)
             for i in range(len(names)):
-                #f.write(names[i]+'\t')
                 f.write(names[i]+'\t')
-                for j in range(len(lines[i])):
-                    f.write(lines[i][j])
-                    if j!=len(lines[i])-1:
-                        f.write(',')
-                    else:
-                        f.write('\n')
+                f.write(','.join(lines[i]) + '\n')
+
     else:
         with open(output, 'w') as f:
             for line in lines:
-                for j in range(len(line)):
-                    f.write(line[j])
-                    if j==len(line)-1:
-                        f.write('\n')
-                    else:
-                        f.write(' ')
-               
+                f.write(' '.join(line) + '\n')
 
 if __name__ == '__main__':
     names, lines, deleted_names, deleted = preprocess(args.file_name, args.convert)
-    write_output(names, lines, args.output, args.convert)
-    write_output(deleted_names, deleted, args.deleted_path, False)
+    dedup_path = os.path.join(args.output_dir, 'dedup.txt')
+    deleted_path = os.path.join(args.output_dir, 'deleted.txt')
+    write_output(names, lines, dedup_path, args.convert)
+    write_output(deleted_names, deleted, deleted_path, False)
     
