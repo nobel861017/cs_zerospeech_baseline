0ba36c95e6377f8d914cf1e1ac538d2687dc580e
nobel861017 <nobel861017@yahoo.com.tw>
2023-08-17 23:45:36 +0800

    modify quantize_dedup.sh

--- quantize_dedup.sh
+++ quantize_dedup.sh
@@ -1,32 +1,36 @@
-exp_name=exp_name
-config_path=scripts/config/config_train_debug.yaml
-train_output_dir=exp_name/train
+exp_name=half_cs_half_mono_km_100c_30it
+num_workers=2
 
-num_workers=16
+# quantize training set (8 sets: cs_es-en, cs_fr-en, cs_zh-en, mono_es, mono_fr, mono_zh, mono_en)
+config_path=scripts/config/config_train.yaml
+train_output_dir=$exp_name/train
 
 python scripts/quantize_audio.py $train_output_dir --config $config_path
 python scripts/deduplicate.py --output $train_output_dir --max_units 512 --convert
 
-
-config_path=scripts/config/config_dev_debug.yaml
+# quantize dev set (3 sets)
+config_path=scripts/config/config_dev.yaml
 dev_output_dir=$exp_name/dev
 
 python scripts/quantize_audio.py $dev_output_dir --config $config_path
 python scripts/deduplicate.py --output $dev_output_dir --max_units 512 --convert
 
-config_path=scripts/config/config_test_correct_debug.yaml
+# quantize test correct set (3 sets)
+config_path=scripts/config/config_test_correct.yaml
 test_output_correct_dir=$exp_name/test/correct
 
 python scripts/quantize_audio.py $test_output_correct_dir --config $config_path
 python scripts/deduplicate.py --output $test_output_correct_dir --max_units 512 --convert
 python scripts/deduplicate.py --output $test_output_correct_dir --max_units 512
 
-config_path=scripts/config/config_test_wrong_debug.yaml
+# quantize test wrong set (3 sets)
+config_path=scripts/config/config_test_wrong.yaml
 test_output_wrong_dir=$exp_name/test/wrong
 
 python scripts/quantize_audio.py $test_output_wrong_dir --config $config_path
 python scripts/deduplicate.py --output $test_output_wrong_dir --max_units 512
 
+# preprocess data
 fairseq-preprocess --only-source \
     --trainpref $train_output_dir/dedup_converted.txt \
     --validpref $dev_output_dir/dedup_converted.txt \
