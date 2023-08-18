a9093609e47c39a9b4508926eb6469747c929642
cky1124 <kevinyang1124@gmail.com>
2023-08-18 16:06:26 +0800

    unit LM training script

---
+++ train_unit.sh
@@ -0,0 +1,27 @@
+exp_name=kmeans_mono_25hr
+run_id=unitLM
+max_update=50000
+total_num_update=50000
+warmup_update=5000
+max_positions=512
+max_tokens=20480
+num_workers=32
+update_freq=16
+
+export WANDB_RUN_ID=$run_id
+
+fairseq-train --fp16 $exp_name/bin \
+    --task masked_lm --criterion masked_lm \
+    --save-dir $exp_name/checkpoints/ \
+    --keep-last-epochs 3 \
+    --train-subset train \
+    --num-workers $num_workers \
+    --arch roberta_base \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-06 --clip-norm 0.0 \
+    --lr-scheduler polynomial_decay --lr 0.0005 --total-num-update $total_num_update --warmup-updates $warmup_update \
+    --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+    --mask-multiple-length 10 --mask-prob 0.5 --mask-stdev 10 \
+    --sample-break-mode eos --tokens-per-sample 3072 --max-positions $max_positions \
+    --max-tokens $max_tokens --update-freq $update_freq --max-update $max_update \
+    --seed 5 --log-format simple --log-interval 100 --skip-invalid-size-inputs-valid-test \
+    --ddp-backend legacy_ddp --wandb-project $exp_name --fp16
