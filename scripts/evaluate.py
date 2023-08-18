ac27045ca1959956f8fd9619523665f0f1beecad
cky1124 <kevinyang1124@gmail.com>
2023-08-18 16:12:57 +0800

    modified evaluation script for accuracy for language and set

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -16,24 +16,32 @@ with open(args.input, 'r') as f:
         filename = Path(filename)
 
         idx = str(os.path.splitext(os.path.basename(filename))[0])
-        top_dir = os.path.dirname(os.path.dirname(filename))
+        sets = os.path.basename(os.path.dirname(os.path.dirname(filename)))
+        sets_dir = os.path.dirname(os.path.dirname(filename))
+        lang = os.path.basename(os.path.dirname(sets_dir))
         correct_or_wrong = os.path.basename(os.path.dirname(filename))
 
-        if top_dir not in table.keys():
-            table[top_dir] = dict()
+        if lang not in table.keys():
+            table[lang] = dict()
+        if sets not in table[lang].keys():
+            table[lang][sets] = dict()
         
-        if idx not in table[top_dir].keys():
-            table[top_dir][idx] = dict()
+        if idx not in table[lang][sets].keys():
+            table[lang][sets][idx] = dict()
 
-        table[top_dir][idx][correct_or_wrong] = prob
+        table[lang][sets][idx][correct_or_wrong] = prob
 
-correct_counter = 0
-total = 0
-for top_dir in table.keys():
-    for idx in table[top_dir].keys():
-        if table[top_dir][idx]["correct"] >= table[top_dir][idx]["wrong"]:
-            correct_counter += 1
-        total += 1
+for lang in table.keys():
+    for sets in table[lang].keys():
+        correct_counter = 0
+        total = 0
+        for idx in table[lang][sets].keys():
+            assert ("correct" in table[lang][sets][idx].keys() and "wrong" in table[lang][sets][idx].keys()), "Missing some data!"
+            if table[lang][sets][idx]["correct"] >= table[lang][sets][idx]["wrong"]:
+                correct_counter += 1
+            total += 1
 
-print(correct_counter / total)
+        print(f'Accuracy of {lang} {sets}: {correct_counter / total}')
+
+print('Finished evaluation.')
         
