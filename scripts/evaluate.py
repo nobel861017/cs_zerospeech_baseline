e866a9473c047af49a32c8b49fec4ee0c68ce315
cky1124 <kevinyang1124@gmail.com>
2023-08-23 18:52:58 +0800

    Evaluation script that will skip invalid sample

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 import os
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', type=str, help='The input file of pseudo-probability')
 args = parser.parse_args()
+
+valid_table = dict()
+valid_table['es_en'] = dict()
+df_es_en = pd.read_csv("/work/b08202033/zerospeech2021_baseline/scripts/test_cs_es-en_labeled.csv", encoding="utf-8")
+valid_table['es_en']['test'] = list(map(int, list(df_es_en["valid"])))
 
 table = dict()
 with open(args.input, 'r') as f:
@@ -35,11 +41,20 @@ for lang in table.keys():
     for sets in table[lang].keys():
         correct_counter = 0
         total = 0
+        valid = None
+        if lang in valid_table.keys() and sets in valid_table[lang].keys():
+            valid = valid_table[lang][sets]
         for idx in table[lang][sets].keys():
             if ("correct" in table[lang][sets][idx].keys() and "wrong" in table[lang][sets][idx].keys()):
-                if table[lang][sets][idx]["correct"] >= table[lang][sets][idx]["wrong"]:
-                    correct_counter += 1
-                total += 1
+                if valid is not None:
+                    if valid[int(idx)]==1:
+                        if table[lang][sets][idx]["correct"] >= table[lang][sets][idx]["wrong"]:
+                            correct_counter += 1
+                        total += 1
+                else:
+                    if table[lang][sets][idx]["correct"] >= table[lang][sets][idx]["wrong"]:
+                        correct_counter += 1
+                    total += 1
 
         print(f'Accuracy of {lang} {sets}: {correct_counter / total}')
 
