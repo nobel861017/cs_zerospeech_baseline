e04646267275f185819e40fbf071483d665f094e
cky1124 <kevinyang1124@gmail.com>
2023-09-15 16:15:22 +0800

    remove unneeded part

--- scripts/cpc/criterion/__init__.py
+++ scripts/cpc/criterion/__init__.py
@@ -2,5 +2,5 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from .criterion import CPCUnsupersivedCriterion, SpeakerCriterion, \
-    PhoneCriterion, NoneCriterion, CTCPhoneCriterion
+# from .criterion import CPCUnsupersivedCriterion, SpeakerCriterion, \
+#     PhoneCriterion, NoneCriterion, CTCPhoneCriterion
